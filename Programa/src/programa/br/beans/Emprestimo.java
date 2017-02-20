2306adaa9b582bc105686bac046301ea8e26780e
augustocp98 <augustocpinheiro@hotmail.com>
2017-02-20 15:04:29 -0300

    Add files via upload

--- Programa/src/programa/br/beans/Emprestimo.java
+++ Programa/src/programa/br/beans/Emprestimo.java
@@ -1,5 +1,46 @@
 package programa.br.beans;
 
 public class Emprestimo {
-
+	private String cpfCliente;
+	private int codigoPano;
+	private String dataRecebimento;
+	private String dataEntrega;
+	
+	public Emprestimo(String cpfCliente, int codigoPano, String dataRecebimento, String dataEntrega) {
+		super();
+		this.cpfCliente = cpfCliente;
+		this.codigoPano = codigoPano;
+		this.dataRecebimento = dataRecebimento;
+		this.dataEntrega = dataEntrega;
+	}
+	
+	
+	public String getCpfCliente() {
+		return cpfCliente;
+	}
+	public void setCpfCliente(String cpfCliente) {
+		this.cpfCliente = cpfCliente;
+	}
+	public int getCodigoPano() {
+		return codigoPano;
+	}
+	public void setCodigoPano(int codigoPano) {
+		this.codigoPano = codigoPano;
+	}
+	public String getDataRecebimento() {
+		return dataRecebimento;
+	}
+	public void setDataRecebimento(String dataRecebimento) {
+		this.dataRecebimento = dataRecebimento;
+	}
+	public String getDataEntrega() {
+		return dataEntrega;
+	}
+	public void setDataEntrega(String dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+	
+	
+	
+	
 }
--- Programa/src/programa/br/persistence/EmprestimoDAO.java
+++ Programa/src/programa/br/persistence/EmprestimoDAO.java
@@ -1,63 +1,91 @@
 package programa.br.persistence;
-import programa.br.beans.Cliente;
-import programa.br.beans.Pano;
-public class EmprestimoDAO {
-	String cpfCliente;
-	int codigoPano;
-	String dataRecebimento;
-	String dataEntrega;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import programa.br.beans.Cliente;
+import programa.br.beans.Emprestimo;
+import programa.br.beans.Pano;
+
+public class EmprestimoDAO {
+	private static final String NOME_ARQUIVO = "emprestimo.txt";
+	static ArrayList<Emprestimo> emprestimos = new ArrayList();
 
 	public void emprestar(int cod,String cpf){
 		ClienteDAO cdao = new ClienteDAO();
-		//PanoDAO pdao = new PanoDAO();
+		PanoDAO pdao = new PanoDAO();
 		Cliente c = cdao.buscarPorCpf(cpf); 	
-		//Pano p = pdao.buscarPorCodigo(cod);	
-		//p.setCliente(c);	
+		Pano p = pdao.procurarPorCodigo(cod);	
+		p.setCliente(c);
+		p.setEmprestado(true);
+	}
+	public void escreverArquivo(){
+		File f = new File(NOME_ARQUIVO);
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+
+		try {
+			fw = new FileWriter(f);
+			bw = new BufferedWriter(fw);
+			for (Emprestimo emprestimo : emprestimos) {
+				bw.write(emprestimo.getCpfCliente()+";"+emprestimo.getCodigoPano()+";"+emprestimo.getDataRecebimento()+";"+emprestimo.getDataEntrega());
+				bw.newLine();
+			}
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}finally{
+			try {
+				fw.close();
+				bw.close();
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
 
 	}
 
+	static{
+		File f = new File(NOME_ARQUIVO);
+		FileReader fr = null;
+		BufferedReader br = null;
 
-	public String getCpfCliente() {
-		return cpfCliente;
+		try {
+			fr = new FileReader(f);
+			br = new BufferedReader(fr);
+
+			String linha;
+			while( (linha = br.readLine()) != null ) {
+				String[] dados = linha.split(";");
+				Emprestimo e = new Emprestimo(dados[0],Integer.parseInt(dados[1]),dados[2],dados[3]);
+				emprestimos.add(e);
+			}
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				fr.close();
+				br.close();
+
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+
+
+		}
+
 	}
-
-
-	public void setCpfCliente(String cpfCliente) {
-		this.cpfCliente = cpfCliente;
-	}
-
-
-	public int getCodigoPano() {
-		return codigoPano;
-	}
-
-
-	public void setCodigoPano(int codigoPano) {
-		this.codigoPano = codigoPano;
-	}
-
-
-	public String getDataRecebimento() {
-		return dataRecebimento;
-	}
-
-
-	public void setDataRecebimento(String dataRecebimento) {
-		this.dataRecebimento = dataRecebimento;
-	}
-
-
-	public String getDataEntrega() {
-		return dataEntrega;
-	}
-
-
-	public void setDataEntrega(String dataEntrega) {
-		this.dataEntrega = dataEntrega;
-	}
-
-
-
 }
 
